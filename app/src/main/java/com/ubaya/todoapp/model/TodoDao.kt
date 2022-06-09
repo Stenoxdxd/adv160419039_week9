d97aa154102bebeeab736edaf257a912c0ec3a8a
BryanD <vercelli.bryan92@gmail.com>
2022-06-09 21:05:25 +0700

    week 8+9

---
+++ app/src/main/java/com/ubaya/todoapp/model/TodoDao.kt
@@ -0,0 +1,19 @@
+package com.ubaya.todoapp.model
+
+import androidx.room.*
+
+@Dao
+interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg todo:Todo)
+    @Query("SELECT * FROM todo ORDER BY priority DESC")
+    suspend fun selectAllTodo(): List<Todo>
+    @Query("SELECT * FROM todo WHERE uuid = :id")
+    suspend fun selectTodo(id:Int): Todo
+    @Delete
+    suspend fun deleteTodo(todo:Todo)
+    @Query("Update todo SET title:title, notes=:notes, priority=:priority WHERE uuid=:id")
+    suspend fun update(title:String, notes:String, priority:Int, id:Int)
+    @Query("Update todo SET is_done = 1 WHERE uuid=:id")
+    suspend fun updateTodo(todo:Todo)
+}
